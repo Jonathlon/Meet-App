810894747b0813a8f6245e42ad517958688a342d
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-07-24 11:29:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useState } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
-import "./App.css";
 import NumberOfEvents from "./components/NumberOfEvents";
+import { getEvents } from "./api";
+
+import "./App.css";
+
+const [events, setEvents] = useState([]);
+const [currentNOE, setCurrentNOE] = useState(32);
 
 const App = () => {
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
       <NumberOfEvents />
+      <EventList events={events} />
     </div>
   );
 };
